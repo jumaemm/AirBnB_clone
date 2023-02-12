2553634eebee6b87ba9e2124122f742764c4bb5b
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-02-12 19:35:19 +0000

    Import additional classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,15 +8,18 @@ from models.base_model import BaseModel
 from models.user import User
 from models.city import City
 from models.state import State
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 CLASSES = {
         "BaseModel": BaseModel,
         "User": User,
         "City": City,
-        #"Place": Place,
+        "Place": Place,
         "State": State,
-        #"Amenity": Amenity,
-        #"Review": Review
+        "Amenity": Amenity,
+        "Review": Review
     }
 
 
